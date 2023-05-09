318e90b428ef0513bf7c63a583aa13ad6f4fe580
JUWONCALEB <63285430+juwoncaleb@users.noreply.github.com>
2023-05-09 12:05:47 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,7 @@ import '../styles/globals.css'
 import Head from 'next/head'
 function MyApp({ Component, pageProps }) {
   return (
-    <>
+    <> 
     <Head>
     <link rel="icon" href="https://ik.imagekit.io/juwoncaleb/ezgif.com-gif-maker__10__zn7YAAwIR.webp?updatedAt=1682516186976" />
     </Head>
